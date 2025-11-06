b44379e00e6b126e0095ad35cce4484a77075478
-Shinnamon- <chimonkung0@gmail.com>
2025-11-06 22:21:54 +0700

    try to commit by myself

--- course1/src/script/script.js
+++ course1/src/script/script.js
@@ -1,29 +1,31 @@
-let count = 0;
-let fruit = ["banana"]
+// let count = 0;
+// let fruit = ["banana"]
 
 
-document.getElementById("btn").onclick = function() {
-    // document.getElementById("title").textContent = "Hello Isus yes mae";
-    // count = count + 1
-    // console.log(`Click ${count}`)
-    // if (count === 5) {
-    //     console.log("E dog");
-    //     callMae("Shinnamon")
-    //     count = 0;
-    // }
+// document.getElementById("btn").onclick = function() {
+//     // document.getElementById("title").textContent = "Hello Isus yes mae";
+//     // count = count + 1
+//     // console.log(`Click ${count}`)
+//     // if (count === 5) {
+//     //     console.log("E dog");
+//     //     callMae("Shinnamon")
+//     //     count = 0;
+//     // }
 
-    console.log(fruit)
-};
+//     console.log(fruit)
+// };
 
-document.getElementById("btnAdd").onclick = function() {
-    fruit.push("apple")
-    console.log(fruit)
-}
+// document.getElementById("btnAdd").onclick = function() {
+//     fruit.push("apple")
+//     console.log(fruit)
+// }
 
-document.getElementById("btnPop").onclick = function() {
-    fruit.pop("banana")
-    console.log(fruit)
-}
-const callMae = (name) => {
-    alert(`${name} Mae mung mah!!!!`)
-}
+// document.getElementById("btnPop").onclick = function() {
+//     fruit.pop("banana")
+//     console.log(fruit)
+// }
+// const callMae = (name) => {
+//     alert(`${name} Mae mung mah!!!!`)
+// }
+
+
